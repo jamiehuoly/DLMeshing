5d0a3b13a7e78b7931ef2eac98462700834e905d
jamiehuoly <jamiehuoly@gmail.com>
2025-12-18 18:47:32 +0800

    formatting codes

--- Codes/GNN.py
+++ Codes/GNN.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-from torch_geometric.nn import MetaLayer, EdgeConv
+from torch_geometric.nn import EdgeConv
 
 
 class MeshOptimizerGNN(nn.Module):
---
+++ Codes/main.py
@@ -0,0 +1,12 @@
+import gmsh
+import gmsh_to_python
+
+# model import and initial 3D mesh generation
+gmsh.initialize()
+gmsh.model.add("elbow")
+gmsh.open("elbow.step")
+gmsh.model.mesh.generate(3)
+
+# results from transform
+coordinate, elems_numpy = gmsh_generation.gmsh_tag_transform()
+print(coordinate, elems_numpy)

b9d76c480f3e5288b03e934c8befa0c20df2750b
jamiehuoly <jamiehuoly@gmail.com>
2025-12-22 17:50:38 +0800

    modifying logs

--- Codes/edges.py
+++ Codes/edges.py
@@ -22,12 +22,13 @@ def get_graph_edges(coordinate, elems_numpy):
     edges_stacked = np.vstack(all_edges)
     edges_stacked_transpose = edges_stacked.transpose()
     edge_index = torch.tensor(edges_stacked_transpose, dtype=torch.long)
-    print(edge_index)
-    print(edge_index.shape)
+    # print(edge_index)
+    # print(edge_index.shape)
+
     # undirected and eliminate repeated edges
     edge_index = to_undirected(edge_index)
-    print(edge_index)
-    print(edge_index.shape)
+    # print(edge_index)
+    # print(edge_index.shape)
     edge_index = coalesce(edge_index)
 
     return edge_index
--- Codes/main.py
+++ Codes/main.py
@@ -1,4 +1,6 @@
 import gmsh
+from torch_geometric.utils import is_undirected
+
 import gmsh_to_numpy
 import edges
 import utils
@@ -12,12 +14,14 @@ gmsh.model.mesh.generate(3)
 # 2. transformed results
 coordinate, elems_numpy = gmsh_to_numpy.gmsh_tag_transform()
 # print(coordinate, elems_numpy)
-# print(elems_numpy.shape)
+# print(coordinate.shape)
 
 # 3. build torch_geometric data for NN
 edges_index = edges.get_graph_edges(coordinate, elems_numpy)
+print(is_undirected(edges_index))
+
 graph_data = utils.create_tg_data(coordinate, edges_index)
-# print(edges_index[0])
+print(edges_index)
 print(graph_data)
 
 

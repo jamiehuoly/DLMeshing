0b0dc449794bd793aeda06cb010d4c1cf9811ff9
Jamie <jamiehuoly@gmail.com>
2026-01-03 16:23:36 +0800

    整合到utils里

--- Codes/main.py
+++ Codes/main.py
@@ -1,8 +1,6 @@
 import gmsh
 from torch_geometric.utils import is_undirected
 
-import gmsh_to_numpy
-import edges
 import utils
 
 # 1. model import and initial 3D mesh generation
@@ -12,12 +10,12 @@ gmsh.open("elbow.step")
 gmsh.model.mesh.generate(3)
 
 # 2. transformed results
-coordinate, elems_numpy = gmsh_to_numpy.gmsh_tag_transform()
+coordinate, elems_numpy = utils.gmsh_tag_transform()
 # print(coordinate, elems_numpy)
 # print(coordinate.shape)
 
 # 3. build torch_geometric data for NN
-edges_index = edges.get_graph_edges(coordinate, elems_numpy)
+edges_index = utils.get_graph_edges(coordinate, elems_numpy)
 print(is_undirected(edges_index))
 
 graph_data = utils.create_tg_data(coordinate, edges_index)
--- Codes/utils.py
+++ Codes/utils.py
@@ -1,7 +1,77 @@
+import gmsh
+import numpy as np
 import torch
 from torch_geometric.data import Data
+from torch_geometric.utils import to_undirected, coalesce
 
 def create_tg_data(x, edge_index):
     coordinate_tensor = torch.tensor(x, dtype=torch.float)
     graph_data = Data(x=coordinate_tensor, edge_index=edge_index)
     return graph_data
+
+# from gmsh-stype to numpy-style
+def gmsh_tag_transform():
+    # node_tag: node ID list
+    # coordinate: [x1, y1, z1, x2, y2, z2, ...] flattened list
+    node_tag, coordinate, _ = gmsh.model.mesh.getNodes()
+
+    # reshape to 3-dimension
+    coordinate = np.array(coordinate, dtype=np.float32).reshape(len(node_tag), 3)
+
+    # Mirror relation： Gmsh tag -> Python index; Gmsh tag may not continuous
+    node_index = {tag: i for i, tag in enumerate(node_tag)}
+
+    element_types, element_tags, node_tags_per_element = gmsh.model.mesh.getElements(dim=3)
+    # print(gmsh.model.mesh.getElements(dim=3))
+    # print(type(elementTypes))
+    # print(elementTags)
+    # print(nodeTagsPerEl)
+
+    # 4 means tetrahedral (4 nodes)
+    if 4 not in element_types:
+        raise ValueError("No tetrahedron found! Please check if 3D mesh was generated.")
+
+    tetra_index = element_types.tolist().index(4)
+    tetra_nodes_tags = np.array(node_tags_per_element[tetra_index], dtype=np.int64).reshape(-1, 4)
+    print(tetra_nodes_tags)
+
+    # mirroring gmsh coordinate into pytorch coordinate
+    elems_numpy = np.zeros_like(tetra_nodes_tags)
+    for i in range(tetra_nodes_tags.shape[0]):
+        for j in range(tetra_nodes_tags.shape[1]):
+            gmsh_tag = tetra_nodes_tags[i, j]
+            python_index = node_index[gmsh_tag]
+            elems_numpy[i, j] = python_index
+
+    print(f"Finished transferring: {coordinate.shape[0]} Nodes, {elems_numpy.shape[0]} Tetras")
+    return coordinate, elems_numpy
+
+def get_graph_edges(coordinate, elems_numpy):
+    # tetrahedral edges combinations
+    all_edges = []
+    combinations = [
+        [0, 1],
+        [0, 2],
+        [0, 3],
+        [1, 2],
+        [1, 3],
+        [2, 3],
+    ]
+    # iterate combinations to extract tags for each edge
+    for i, j in combinations:
+        edge_pair = elems_numpy[:, [i, j]]
+        all_edges.append(edge_pair)
+
+    edges_stacked = np.vstack(all_edges)
+    edges_stacked_transpose = edges_stacked.transpose()
+    edge_index = torch.tensor(edges_stacked_transpose, dtype=torch.long)
+    # print(edge_index)
+    # print(edge_index.shape)
+
+    # undirected and eliminate repeated edges
+    edge_index = to_undirected(edge_index)
+    # print(edge_index)
+    # print(edge_index.shape)
+    edge_index = coalesce(edge_index)
+
+    return edge_index

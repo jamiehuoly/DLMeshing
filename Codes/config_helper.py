d6a0f16da89d24c2f0c1f6144e9ad92f6655c527
zhuo <zhiyu.huo@uzh.com>
2026-01-27 14:11:06 +0100

    Adding config_helper.py to help customers see their surface ids

---
+++ Codes/config_helper.py
@@ -0,0 +1,55 @@
+import os
+import sys
+
+import gmsh
+
+
+def visualise_surfaces(geometry_file):
+    if not os.path.exists(geometry_file):
+        print(f"Error! Could not find geometry file: {geometry_file}")
+        return
+
+    gmsh.initialize()
+    gmsh.open(geometry_file)
+    surfaces = gmsh.model.getEntities(2)
+    print("\n" + "-" * 60)
+    print("Now providing auto detected predictions:")
+    for dim, tag in surfaces:
+        stype = gmsh.model.getType(dim, tag)
+        role = "Wall?"
+        if stype == "Plane":
+            role = "Inlet/Outlet?"
+
+        if stype == "Plane":
+            print(f"\033[1;32m{tag:<12} | {stype:<10} | {role}\033[0m")  # Green font
+        else:
+            print(f"{tag:<12} | {stype:<10} | {role}")
+            pass
+
+    gmsh.option.setNumber("Geometry.Surfaces", 1)  # 必须显示面
+    gmsh.option.setNumber("Geometry.SurfaceType", 2)  # 2 = Filled (实体填充)
+    gmsh.option.setNumber("Geometry.Lines", 1)  # 显示边框线
+    gmsh.option.setNumber("Geometry.SurfaceLabels", 1)  # 开启 ID 显示
+    gmsh.option.setNumber("General.BackgroundGradient", 0)  # 关闭渐变背景
+    gmsh.option.setColor("General.Background", 50, 50, 50)  # 深灰色背景
+    gmsh.option.setColor("Geometry.Color.Lines", 0, 0, 0)# 线条设为 黑色 (增加轮廓感)
+
+    # 屏蔽日志
+    gmsh.option.setNumber("General.Verbosity", 0)
+
+    print("\n" + "Window opening to show the geometry... Feel free to rotate the model.")
+    print("Clicking 'Tools' -> 'Visibility' to view more detailed surface ids.")
+    gmsh.fltk.run()
+    gmsh.finalize()
+
+if __name__ == "__main__":
+    # python config_helper.py model.step
+    if len(sys.argv) > 1:
+        input_file = sys.argv[1]
+        visualise_surfaces(input_file)
+    else:
+        default_file = "elbow.step"
+        if os.path.exists(default_file):
+            visualise_surfaces(default_file)
+        else:
+            print("Geometry file is not provided. Please use 'python config_helper.py model.step' to run the script.")

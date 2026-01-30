cbaf887485ec6fef24d607a892be3516f3407c5f
Jamie <jamiehuoly@gmail.com>
2026-01-31 07:40:20 +0800

    添加real_main.py主脚本，归纳整理完整运行逻辑 修复bug

--- Codes/real_main.py
+++ Codes/real_main.py
@@ -9,9 +9,9 @@ from position_to_gmsh import generate_mesh_from_pos
 
 GENERATE_FINE_MESH = True
 CONFIG_FILE = "case_config.json"
-WORKING_DIRECTORY = "case_working"
 MESH_FILE_NAME = "elbow.msh"
-VTK_FILE_PATTERN = "VTK_fine/DLMeshing_fine_*.vtk"
+INFER_VTK_FILE_PATTERN = "VTK_Coarse/DLMeshing_coarse_*.vtk"
+TRAINING_VTK_FILE_PATTERN = "VTK_fine/DLMeshing_fine_*.vtk"
 GRAPH_DATA_FILE_NAME = "ground_truth_graph.pt"
 MODEL_SAVE_PATH = "trained_models"
 MODEL_SAVE_NAME = "gnn_model.pth"
@@ -25,6 +25,14 @@ if len(sys.argv) > 1:
 else:
     MODE = "inference"
 
+config = utils.get_config_json(CONFIG_FILE)
+if not config["work_dir"]:
+    print(f"work_dir is not defined! Please define in file: {CONFIG_FILE}!")
+    utils.safe_exit()
+else:
+    if not os.path.exists(config["work_dir"]):
+        os.makedirs(config["work_dir"])
+
 # read config file & generate coarse mesh
 generate_coarse = generate_mesh_from_geometry(CONFIG_FILE)
 if not generate_coarse:
@@ -33,10 +41,11 @@ if not generate_coarse:
 
 ## ToDo: OpenFOAM
 
-vtk_file = utils.get_latest_vtk(VTK_FILE_PATTERN)
+vtk_file = utils.get_latest_vtk(INFER_VTK_FILE_PATTERN)
 print(f"VTK File Name: {vtk_file}")
 
 ## ToDo: Training
+# vtk_file = utils.get_latest_vtk(TRAINING_VTK_FILE_PATTERN)
 # x_features, edge_index, y, pos, L_Char, raw_pos = utils.process_vtk_to_graph(vtk_file, MODE)
 # graph_data = utils.create_tg_data(x_features, edge_index, y, pos)
 # graph_data.L_Char = torch.tensor(L_Char, dtype=torch.float)
@@ -45,7 +54,6 @@ print(f"VTK File Name: {vtk_file}")
 # print(f"Saved graph data to path: {target_path}")
 
 # Inference
-config = utils.get_config_json(CONFIG_FILE)
 pos_output_path = os.path.join(config["work_dir"], POS_FILE_NAME)
 generate_size_field(vtk_file, pos_output_path, MODE)
 generate_mesh_from_pos(config, pos_output_path)

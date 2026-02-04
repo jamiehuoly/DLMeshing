2ba45299ba0ea17c3d581ee10839fc9630198397
Jamie <jamiehuoly@gmail.com>
2026-02-04 17:44:30 +0800

    修复bug

--- Codes/foam_operator.py
+++ Codes/foam_operator.py
@@ -173,7 +173,7 @@ class OpenFoamAutomator:
         Modify settings by using foamDictionary
         """
         # 构造命令: foamDictionary 0/U -entry boundaryField.inlet.type -set fixedValue
-        cmd = f"foamDictionary {file_rel_path} -entry {entry} -set {str(value)}"
+        cmd = f"foamDictionary {file_rel_path} -entry {entry} -set '{str(value)}'"
         self._run_cmd(cmd, log_name="foamDictionary")
 
     def _test_foam_entry(self, file_rel_path, entry):
